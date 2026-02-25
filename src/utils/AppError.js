6e45f51d7e99c0ea301e94b7367740ea417acfae
Javan Girardi <javan.girardi@gmail.com>
2026-02-25 17:03:39 -0300

    adiciona tratamento de erro global com AppError e ajusta o service

---
+++ src/utils/AppError.js
@@ -0,0 +1,10 @@
+class AppError extends Error {
+    constructor(status, message) {
+        super(message);
+        this.status = status;
+        this.isOperational = true;
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+module.exports = AppError;
