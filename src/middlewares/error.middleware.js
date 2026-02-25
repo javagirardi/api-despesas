1ae70b6612fcfff7dab86a129ca3cbf0aef9441d
Javan Girardi <javan.girardi@gmail.com>
2026-02-25 16:11:14 -0300

    adiciona os middlewares e trata os problemas de auth e de erros. Adiciona dependencia no app.js

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,10 @@
+module.exports = (err, req, res, next) => {
+    console.error("Erro capturado:", err);
+
+    const status = err.status || 500;
+    const message = err.message || "Erro interno do servidor";
+
+    return res.status(status).json({
+        error: message
+    });
+};
