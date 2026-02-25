f53961201bb5156d5d0d8d90c2444d7f34f02069
Javan Girardi <javan.girardi@gmail.com>
2026-02-25 11:39:02 -0300

    cria o BD e insere o arquivo 'schema.sql' no projeto

---
+++ schema.sql
@@ -0,0 +1,35 @@
+/*cria a UUID direto no BD*/
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+/*cria a tabela de despesas*/
+CREATE TABLE despesas (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  solicitante_email TEXT NOT NULL,
+  centro_custo TEXT NOT NULL,
+  descricao TEXT NOT NULL,
+  valor NUMERIC(12,2) NOT NULL,
+  moeda TEXT DEFAULT 'BRL',
+  status TEXT NOT NULL CHECK (
+    status IN ('rascunho','enviado','aprovado','rejeitado')
+  ),
+  criado_em TIMESTAMP DEFAULT NOW(),
+  atualizado_em TIMESTAMP DEFAULT NOW()
+);
+
+/*cria a tabela de aprovações com relacionamento entre tabelas*/
+CREATE TABLE aprovacoes (
+  id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  despesa_id UUID NOT NULL,
+  aprovador_email TEXT NOT NULL,
+  acao TEXT NOT NULL CHECK (
+    acao IN ('aprovado','rejeitado')
+  ),
+  comentario TEXT,
+  criado_em TIMESTAMP DEFAULT NOW(),
+
+  CONSTRAINT fk_despesa
+    FOREIGN KEY (despesa_id)
+    REFERENCES despesas(id)
+    ON DELETE CASCADE
+);
+
